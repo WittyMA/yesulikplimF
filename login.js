5a93c6fcb96cceb20db0a55cff403da40ead39ff
Wisdom K Anyizah <wwanyizah@gmail.com>
2024-07-01 04:36:48 +0000

    Admin for login

--- login.js
+++ login.js
@@ -14,44 +14,42 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   
     // Handle form submission for loginForm
-    document.getElementById('loginForm').addEventListener('submit', async function(e) {
-      e.preventDefault();
-  
-      var username = document.getElementById('username').value.trim();
-      var password = document.getElementById('password').value.trim();
-  
-      if (username === '' || password === '') {
-        alert('Username and password are required.');
-        return;
-      }
-  
-      try {
-        const response = await fetch('/login', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ username, password }),
-        });
-      
-        if (!response.ok) {
-          throw new Error('Network response was not ok.');
+      document.getElementById('loginForm').addEventListener('submit', async function(e) {
+        e.preventDefault();
+    
+        var username = document.getElementById('username').value.trim();
+        var password = document.getElementById('password').value.trim();
+    
+        if (username === '' || password === '') {
+          alert('Username and password are required.');
+          return;
         }
-      
-        const data = await response.json();
-      
-        if (response.ok) {
-          alert(data.message); // Display success message or handle as needed
-          window.location.href = 'https://www.yesulikplimits.com/admin.html'; // Redirect to admin page on success
-        } else {
-          alert(data.error); // Display error message from server
+    
+        try {
+          const response = await fetch('/login', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password }),
+          });
+    
+          if (!response.ok) {
+            throw new Error('Network response was not ok.');
+          }
+    
+          const data = await response.json();
+    
+          if (response.ok) {
+            alert(data.message); // Display success message or handle as needed
+            window.location.href = 'https://www.yesulikplimits.com/admin.html'; // Redirect to admin page on success
+          } else {
+            alert(data.error); // Display error message from server
+          }
+        } catch (error) {
+          console.error('Login error:', error);
+          alert('Login failed. Please try again later.');
         }
-      } catch (error) {
-        console.error('Login error:', error);
-        alert('Login failed. Please try again later.');
-      }
-      
+      });
     });
-  });
-  
-  // Handle logout
+    
