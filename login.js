f7225847525778421fb4888873f36e9f3a5e8c51
Wisdom K Anyizah <wwanyizah@gmail.com>
2024-07-05 10:04:47 +0000

    Good subdomain

--- login.js
+++ login.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     try {
-      const response = await fetch('https://wittyma.yesulikplimits.com/login', {
+      const response = await fetch('/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
