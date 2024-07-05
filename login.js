ec749ae46b7b4f2babf843967f60aa4760a531fe
Wisdom K Anyizah <wwanyizah@gmail.com>
2024-07-05 13:30:33 +0000

    Link update

--- login.js
+++ login.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     try {
-      const response = await fetch('/login', {
+      const response = await fetch('https://www.yesulikplimits.com/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
